4bda2d08494d76f1324b9549b58d5f674497bb71
Carlos Elian <elianro03#gmail.com>
2020-07-16 09:56:42 -0300

    Subindo os arquivos da prova.

---
+++ projeto-final/javaScript.js
@@ -0,0 +1,40 @@
+/*
+6.1 - Vareaves podem mudar o valor, constantes não podem ser mudadas.
+
+6.2 - Comandos de decisão seram execultadas quando forem verdadeiras. ex: if() e else.
+
+6.3 - Loops vão ser execultadas enquanto as condições nao forem atendidas. ex: for, While e switch.
+
+6.4 - Funções é execultada quando ela é chamada. ex: Funticion.
+
+6.5 - Existe escopos globais e locais, os golbais ficam foram das funções, e as locais ficam dentro da função.
+
+6.6 - Outputs: É saída de dados como alert, document.write e console.log.
+      Inputs: É entrada de dados como Prompt.
+
+6.7 - Um array é pra colocar varias informações juntas. ex: {nome: ...., idade: ...}.
+
+6.8 - Objetos são varias coisas juntas que são colacadas dentro do escopo [].
+
+6.9 - camelCase é a primeira palavra com a primeira letra em minuscolo, e a segunda com a primeira maiuscula.
+      PascalCase é todas palavras com a primeira letra maiuscula.
+      Snack_case é tudo em minusculo e separado por "_".
+      Kibab-case é tudo em minusculo separado por "-".
+
+6.10 - Caminho Absoluto é : c:/users/fulano/xpto/exemplo.html
+       Caminho relativo é : ./js/exemplo.js
+
+6.11 - No operador "&&" ambas condições tem que ser verdadeiras.
+       No operador "||" uma condição tem que ser verdadeira.
+       No operador "==" ambas condições tem quer iguais.
+       No operador "!=" essa condição significa que vai ser diferente.
+
+6.12 - EPER é Entender - Planejar - Execultar - Revisar, isso é pra gente errar menos não apenas no nosso trabalho mais na vida tbm.
+
+6.13 - Fila é o primeiro que entra é o primeiro que saí.
+       Pilha o primeiro que entra é o ultmo que saí.
+
+6.14 - CSS é onde que fica os codigos que vai aparer para o cliente. Resumindo é tudo que o cliente vê.
+       JavaScript é onde fica os codigos que comandam tudo por tras do CSS.
+       HTML é a base onde fica tudo, tanto do CSS quanto do JavaScript.
+*/
