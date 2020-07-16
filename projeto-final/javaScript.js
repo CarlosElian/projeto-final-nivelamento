806c3fa3a28451bff678816f50fec47033e58c4a
Carlos Elian <elianro03#gmail.com>
2020-07-16 11:18:35 -0300

    Subindo os arquivos do JavaScript modificado e o projeto final.

--- projeto-final/javaScript.js
+++ projeto-final/javaScript.js
@@ -1,38 +1,58 @@
 /*
 6.1 - Vareaves podem mudar o valor, constantes não podem ser mudadas.
 
+       var a = "Fulano" --> Pode alterar o valor
+       cons a = "Fulano" --> Não pode alterar o valor
+
 6.2 - Comandos de decisão seram execultadas quando forem verdadeiras. ex: if() e else.
 
-6.3 - Loops vão ser execultadas enquanto as condições nao forem atendidas. ex: for, While e switch.
+       if(...){
+          ...
+       }else{}
 
-6.4 - Funções é execultada quando ela é chamada. ex: Funticion.
+6.3 - Loops vão ser execultadas enquanto as condições nao forem atendidas. ex: for(), While() e switch().
+
+6.4 - Funções é execultada quando ela é chamada. ex: Funticion exeplo().
+
+       funticion exemplo(){
+              ...
+       }
+       exemplo();
 
 6.5 - Existe escopos globais e locais, os golbais ficam foram das funções, e as locais ficam dentro da função.
 
 6.6 - Outputs: É saída de dados como alert, document.write e console.log.
       Inputs: É entrada de dados como Prompt.
 
-6.7 - Um array é pra colocar varias informações juntas. ex: {nome: ...., idade: ...}.
+       alert() -> vai mostar um alerta para o cliente.
+       document.write() -> vai mostrar um texto na tela.
+       console.log() -> vai mostrar apenas no console.
+       prompt() -> permite que o cliente digite alguma coisa.
 
-6.8 - Objetos são varias coisas juntas que são colacadas dentro do escopo [].
+6.7 - Um array é uma estrutura de dados que armazena varias informações de forma que elas podem ser encontradas.
+
+6.8 - Objetos é para guardar informacões de uma determinada coisa.
+
+       var usuario = [nome:Joao, idade: 32, ...]
 
 6.9 - camelCase é a primeira palavra com a primeira letra em minuscolo, e a segunda com a primeira maiuscula.
       PascalCase é todas palavras com a primeira letra maiuscula.
       Snack_case é tudo em minusculo e separado por "_".
       Kibab-case é tudo em minusculo separado por "-".
 
-6.10 - Caminho Absoluto é : c:/users/fulano/xpto/exemplo.html
-       Caminho relativo é : ./js/exemplo.js
+6.10 - Caminho Absoluto parte do diretório raiz. ex: c:/users/fulano/xpto/exemplo.html
+       Caminho relativo parte da pasta que voê está. ex: ./js/exemplo.js
 
 6.11 - No operador "&&" ambas condições tem que ser verdadeiras.
        No operador "||" uma condição tem que ser verdadeira.
        No operador "==" ambas condições tem quer iguais.
        No operador "!=" essa condição significa que vai ser diferente.
 
-6.12 - EPER é Entender - Planejar - Execultar - Revisar, isso é pra gente errar menos não apenas no nosso trabalho mais na vida tbm.
+6.12 - EPER é Entender - Planejar - Execultar - Revisar
+       Isso é pra gente errar menos não apenas no nosso trabalho mais na vida tbm.
 
-6.13 - Fila é o primeiro que entra é o primeiro que saí.
-       Pilha o primeiro que entra é o ultmo que saí.
+6.13 - Fila é o primeiro que entra é o primeiro que saí. ex: fila.shitf()
+       Pilha o primeiro que entra é o ultmo que saí. ex: pilha.pop()
 
 6.14 - CSS é onde que fica os codigos que vai aparer para o cliente. Resumindo é tudo que o cliente vê.
        JavaScript é onde fica os codigos que comandam tudo por tras do CSS.
